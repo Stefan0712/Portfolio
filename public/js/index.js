c8ce6a369d8bee5b1a36cbb68616a3de052cc560
Stefan0712 <s.vladulescu@gmail.com>
2023-03-08 23:02:36 +0200

    Finished added the other windows

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,20 @@
-const toggleProjectSkills = (id) =>{
-    let list = document.getElementById(id)
-    let arrow = document.getElementById(`arrow-${id}`)
-    list.classList.toggle('showSkills')
-    arrow.classList.toggle('rotateArrow')
-}
-const closeShowMore = (id) =>{
-    const element = document.getElementById(`long-description-${id}`)
-    element.classList.remove('show-long-description')
-}
-const showMore = (id) =>{
-    const element = document.getElementById(`long-description-${id}`)
-    element.classList.add('show-long-description')
-}
+const showWindow = (className) =>{
+    const window = document.querySelector(`.${className}`)
+    window.classList.add('show-window')
+} 
+const closeWindow = (className) =>{
+    const window = document.querySelector(`.${className}`)
+    window.classList.remove('show-window')
+} 
+const toggleWindow = (className) =>{
+    const window = document.querySelector(`.${className}`)
+    if(className==='personal-info'){
+        window.classList.toggle('show-personal-info')
+    }else if(className==='skills'){
 
-const showFullScreenImageModal = (src) =>{
-    const modal = document.getElementById('full-image-modal')
-    const modalImage = document.getElementById('modal-image')
-    modalImage.setAttribute('src',src)
-    modal.classList.add('show-full-image')
-}
-const closeFullScreenImageModal = () =>{
-    const modal = document.getElementById('full-image-modal')
-    modal.classList.remove('show-full-image')
-}
+        window.classList.toggle('show-skills')
+    }else if(className==='certificates'){
 
-const toggleSidePanel = () =>{
-    const sidePanel = document.getElementById('sidePanel')
-    const arrow = document.getElementById('side-panel-arrow')
-    sidePanel.classList.toggle('hide-side-panel')
-    arrow.classList.toggle('rotate-side-panel-arrow')
+        window.classList.toggle('show-certificates')
+    }
 }
